8ba965ff13f6578a6faaeb57cbc3a52cff3bbd1b
John <JayceWyce@protonmail.com>
2023-03-29 21:24:32 -0700

    Sunny's file

--- googlebucket.js
+++ googlebucket.js
@@ -1,19 +1,95 @@
-//googlebucket.js
-/*
-	This javascript file is used to abstract the google bucket management away from
-	the main processing code.
-*/
 
-const storage = require('@google-cloud/storage');
+// Import the Google Cloud client library
+const {Storage} = require('@google-cloud/storage');
+
 
 // Uploads a file to a bucket
-async function uploadFile(bucketName, filePath, destinationPath, bucket_provider) {
-	// file pass the actual file
-	// bucket_provider takes the form {projectID, keyFileName}
+async function uploadFile(bucketName, fileName, fileData, bucket_provider) {
 
-	const bucket = storage(bucket_provider);
-	const options = {destination: destinationPath};
+  var storage = new Storage(bucket_provider);
 
-	await storage.bucket(bucketName).upload(filePath, options);
-	console.log(`File ${filePath} uploaded to bucket ${bucketName} as ${destinationPath}.`);
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(fileName);
+
+
+  const stream = file.createWriteStream({
+    resumable: false,
+    contentType: 'application/octet-stream',
+  });
+
+  // Pass the file data to the stream
+  stream.write(fileData);
+
+  stream.on('error', (err) => {
+    console.error(err);
+  });
+
+  stream.on('finish', () => {
+    console.log(`File ${fileName} uploaded to ${bucketName}.`);
+  });
+
+  // Close the stream to ensure that all data is flushed to the file
+  stream.end();
+
+  storage = null;
+}
+
+
+// Read the file from a bucket
+async function readFile(bucketName, fileName, bucket_provider) {
+  var storage = new Storage(bucket_provider);
+
+  const file = storage.bucket(bucketName).file(fileName);
+
+  const [contents] = await file.download();
+  console.log('File contents:', contents.toString());
+
+  storage = null;
+  return contents.toString();
+}
+
+
+//Update a file in the bucket
+async function updateFile(bucketName, fileName, bucket_provider) {
+  var storage = new Storage(bucket_provider);
+
+  const file = storage.bucket(bucketName).file(fileName);
+
+  const newContents = 'No 4 times: This is the new content of the prog.c file';
+  await file.save(newContents);
+
+  console.log('File updated.');
+
+  storage = null;
+}
+
+
+//Delete a file from a bucket
+async function deleteFile(bucketName, fileName, bucket_provider) {
+  var storage = new Storage(bucket_provider);
+
+  await storage.bucket(bucketName).file(fileName).delete();
+
+  console.log(`${fileName} deleted from ${bucketName}.`);
+
+  storage = null;
+}
+
+// Check if a bucket exists or not
+async function checkBucketStatus(bucketName, bucket_provider) {
+  var storage = new Storage();
+
+  const bucket = storage.bucket(bucketName);
+  var result;
+
+  try {
+    result = await bucket.getMetadata();
+    console.log(`Bucket ${bucketName} exists and is accessible.`);
+  } catch (err) {
+    console.log(`Bucket ${bucketName} does not exist or is not accessible:`);
+    result = null;
+  }
+
+  storage = null;
+  return result;
 }
