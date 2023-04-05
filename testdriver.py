1a366d31ff3c360494d2a2c7fb811c3eea3a39c3
John <JayceWyce@protonmail.com>
2023-04-05 15:59:31 -0700

    testdriver updates

--- testdriver.py
+++ testdriver.py
@@ -1,37 +1,43 @@
 from bridge import *
-import hashlib as hlib
+import sys
 
-ip = "10.13.37.127"
-port = "3000"
+argv = sys.argv
+
+ip = "35.194.62.230"
+port = "3125"
 
 set_connection(ip, port)
 
-# test number 1: we add a new user to the system
-user_id = None # PLEASE PUT YOUR PUBLIC ADDRESS HERE
-prikeyfile = "" # PLEASE PUT YOUR PRIVATE KEY FILE HERE
-prikey = None
-with open(prikeyfile, 'r') as file:
-    prikey = file.read()
+# user identity
+pub_addr_id = 0xb87854FbbC2f5865855C6193f34AC389e6aCbF10
+prikey = 0xce887c6fc788628ebe74e5b2dbec1ea24c35802bc5bc3108cdb346a9af81178a
 
-add_user(user_id, prikey)
+def test_add_user():
+    res = add_user(pub_addr_id, prikey)
+    print(f"[add user] Functionality returns {res}")
 
-# test number 2: we add a file to the system
-fname = "MySecretPasswords.txt"
-with open(fname, "w") as file:
-    # there may still be an issue if we write binary files
-    val = "Hello World!"
-    file.write(val)
-file = open(fname, "r")
-# testing purposes allows this, but user_hash is wrong in reality
-upload_file(user_id, prikey, fname, file)
-file.close()
+def test_upload_file(filename, contents):
+    with open(filename, "w") as file:
+        file.write(contents)
+    file = open(filename, "r")
+    res = upload_file(pub_addr_id, prikey, filename, file)
+    file.close()
+    print(f"[upload file] Functionality returns {res}")
 
-# test number 3: modify the file and save it again
-with open(fname, "a") as file:
-    val = "\nGoodbye World!"
-    file.write(val)
-file = open(fname, "r")
-upload_file(user_id, prikey, fname, file)
-file.close()
+def test_download_file(filename):
+    res = download_file(pub_addr_id, prikey, filename)
+    print(f"[download file] Functionality returns {res}")
+
+funcmap = {"1":test_add_user, "2":test_upload_file, "3":test_download_file}
+try:
+    if(argv[1]=="1"):
+        funcmap["1"]()
+    elif(argv[1]=="2"):
+        funcmap["2"](argv[2], argv[3])
+    elif(argv[1]=="3"):
+        funcmap["3"](argv[2])
+
+except Exception as err:
+    print(f"Something goofed!\n>>>\n{err}\n<<<\n")
 
 
